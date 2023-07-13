88bd85459a16d388500bb383674090710186259f
Badr Eddin Azmi <1t4chy@gmail.com>
2023-07-13 05:32:46 -0700

    HELLO WORLD

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
 * Retir, : always 0 (success)
 */
 int main(void) {
-	puts("\"Programming is like building a multiligual puzzle");
+	puts("Programming is like building a multiligual puzzle\n");
 	return 0;
 }
