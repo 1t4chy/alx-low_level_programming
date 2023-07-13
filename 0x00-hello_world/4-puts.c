14cfa2c765183a0f50f205fd898b32fcdf8c9cfc
Badr Eddin Azmi <1t4chy@gmail.com>
2023-07-13 08:31:47 -0700

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
- * Hello C
- */
-
-int main(void) 
-{
+/**
+* main - Entry point
+*
+* Retir, : always 0 (success)
+*/
+int main(void) {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return 0;
 }
